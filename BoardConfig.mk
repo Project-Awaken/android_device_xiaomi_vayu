5405202e34608f6829ff58e3d176db12cc4fae4f
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    vayu: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,7 +185,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SEPolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
